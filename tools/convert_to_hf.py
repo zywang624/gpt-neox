18318e79a6e111c46db1ac54672ea4a99d541690
vincent <891922758@qq.com>
2026-04-17 09:38:10 +0000

    feat: update convert_to_hf

--- tools/convert_to_hf.py
+++ tools/convert_to_hf.py
@@ -14,6 +14,7 @@
 
 import os
 import sys
+from typing import List
 
 import yaml
 import argparse
@@ -41,7 +42,7 @@ Please investigate carefully whether your model is compatible with all architect
 
 def load_partitions(
     input_checkpoint_path, mp_partitions, layer_idx
-) -> list[torch.Tensor]:
+) -> List[torch.Tensor]:
     """Returns a list containing all weights in a given layer from a model (across MP partitions)"""
 
     loaded_tp_ranks = [
