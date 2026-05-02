b0b277e8909ed652a72e276e72a51cc533a0ac77
vincent <891922758@qq.com>
2026-05-02 06:19:30 +0000

    feat: run aligned_config for 70m vanilla

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ TS=$(date +%m%d_%H%M%S)
 # python deepy.py train.py pythia-70m-deduped.yml  2>&1 | tee output.txt
 # python deepy.py train.py pythia-70m-deduped_lskv.yml  2>&1 | tee pythia-70m-deduped_lskv_d256_w128_wo_act_bias_output_${TS}.txt
 # python deepy.py train.py pythia-70m-deduped_lskv_d256_w1.yml  2>&1 | tee pythia-70m-deduped_lskv_d256_w1_wo_act_bias_output_${TS}.txt
-python deepy.py train.py pythia-70m-deduped_lskv_d128_w128.yml  2>&1 | tee pythia-70m-deduped_lskv_d128_w128_wo_act_bias_init_output_${TS}.txt
+# python deepy.py train.py pythia-70m-deduped_lskv_d128_w128.yml  2>&1 | tee pythia-70m-deduped_lskv_d128_w128_wo_act_bias_init_output_${TS}.txt
 # python deepy.py train.py pythia-70m-deduped_lskv_d64_w128.yml  2>&1 | tee pythia-70m-deduped_lskv_d64_w128_wo_act_bias_output_${TS}.txt
 # python deepy.py train.py pythia-70m-deduped_lskv_d128_w1.yml  2>&1 | tee pythia-70m-deduped_lskv_d128_w1_wo_act_bias_output_${TS}.txt
 # python deepy.py train.py pythia-70m-deduped_lskv_d256_w96.yml  2>&1 | tee pythia-70m-deduped_lskv_d128_w96_wo_act_bias_output_${TS}.txt
@@ -25,3 +25,7 @@ python deepy.py train.py pythia-70m-deduped_lskv_d128_w128.yml  2>&1 | tee pythi
 # python deepy.py train.py pythia-160m-deduped_lskv_d192_w128.yml  2>&1 | tee pythia-160m-deduped_lskv_d192_w128_wo_act_bias_output_${TS}.txt
 
 # python deepy.py train.py pythia-410m-deduped_lskv_d256_w128.yml  2>&1 | tee pythia-410m-deduped_lskv_d256_w128_wo_act_bias_output_${TS}.txt
+
+# python deepy.py train.py pythia-70m-deduped_lskv_d128_w0_wo_act_bias.yml  2>&1 | tee pythia-70m-deduped_lskv_d128_w0_wo_act_bias_output_${TS}.txt
+
+python deepy.py train.py pythia-70m-deduped.yml  2>&1 | tee pythia-70m-deduped_vanilla_aligned_config_output_${TS}.txt
