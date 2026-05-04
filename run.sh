87e5fa79bff79ebec8365f300fdccb4f30424ebe
vincent <891922758@qq.com>
2026-05-04 17:43:14 +0000

    feat: update before switching back to 19k steps

--- run.sh
+++ run.sh
@@ -2,8 +2,14 @@ TS=$(date +%m%d_%H%M%S)
 
 # python deepy.py train.py pythia-70m-deduped.yml  2>&1 | tee pythia-70m-deduped_vanilla_aligned_config_output_${TS}.txt
 
-python deepy.py train.py pythia-70m-deduped_lskv_d128_w128.yml  2>&1 | tee pythia-70m-deduped_lskv_d128_w128_aligned_config_output_${TS}.txt
+# python deepy.py train.py pythia-70m-deduped_lskv_d128_w128.yml  2>&1 | tee pythia-70m-deduped_lskv_d128_w128_aligned_config_output_${TS}.txt
 
 # python deepy.py train.py pythia-70m-deduped_lskv_d128_w0.yml  2>&1 | tee pythia-70m-deduped_lskv_d128_w0_aligned_config_output_${TS}.txt
 
+# python deepy.py train.py pythia-70m-deduped_lskv_d256_w128.yml  2>&1 | tee pythia-70m-deduped_lskv_d256_w128_aligned_config_output_${TS}.txt
 
+# python deepy.py train.py pythia-70m-deduped_lskv_d64_w128.yml  2>&1 | tee pythia-70m-deduped_lskv_d64_w128_aligned_config_output_${TS}.txt
+
+# python deepy.py train.py pythia-70m-deduped_lskv_d256_w0.yml  2>&1 | tee pythia-70m-deduped_lskv_d256_w0_aligned_config_output_${TS}.txt
+
+python deepy.py train.py pythia-70m-deduped_lskv_d1_w128.yml  2>&1 | tee pythia-70m-deduped_lskv_d1_w128_aligned_config_output_${TS}.txt
