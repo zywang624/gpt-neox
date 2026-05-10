8e5c3b439d9a2e0bd0e20e56727a6f9398a0196c
vincent <891922758@qq.com>
2026-05-10 09:48:26 +0000

    feat: update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 TS=$(date +%m%d_%H%M%S)
 
-python deepy.py train.py pythia-70m-deduped_ddim_d128_w128.yml  2>&1 | tee pythia-70m-deduped_ddim_d128_w128_output_${TS}.txt
+python deepy.py train.py pythia-70m-deduped_ddim_d128_w128.yml  2>&1 | tee pythia-70m-deduped_ddim_d128_w128_arc1e-3_output_${TS}.txt
